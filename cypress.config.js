a31f120ec800b524946323959ecd06b8259e18ec
Esra <esraadiller2@gmail.com>
2023-05-03 14:11:12 +0200

    config

--- cypress.config.js
+++ cypress.config.js
@@ -6,4 +6,19 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
+
+  "watchForFileChanges": false, 
+  "chromeWebSecurity": false,
+  "viewportWidth": 1000,
+  "viewportHeight": 6000,
+  "waitForAnimations": true,
+  "animationDistanceThreshold": 20,
+  "defaultCommandTimeout": 6000,
+  "execTimeout": 60000,
+  "pageLoadTimeout": 60000,
+  "requestTimeout": 15000,
+  "responseTimeout": 15000,
+  "video": true,
+  // "failOnStatusCode": false
+
 });
