2882ddc3a8e2496083f52e2cd4d44dee71659bb9
Esra <esraadiller2@gmail.com>
2023-05-03 16:34:20 +0200

    basepage

--- cypress/page-objects/BasePage.js
+++ cypress/page-objects/BasePage.js
@@ -6,4 +6,20 @@ export default class BasePage {
     static logInfo(message) {
         cy.log(message)
     }
+
+    static setMobileViewport() {
+        cy.viewport('iphone-x')
+    }
+
+    static setTabletViewport() {
+        cy.viewport('ipad-2')
+    }
+
+    static setDesktopViewport() {
+        cy.viewport('macbook-13')
+    }
+
+    static setLargeDesktopViewport() {
+        cy.viewport(1980, 1080)
+    }
 }
