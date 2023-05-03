ab2f0f7d608871e607ecc6bdbb1a5254b565de49
Esra <esraadiller2@gmail.com>
2023-05-03 16:39:54 +0200

    navbar

---
+++ cypress/page-objects/components/navbar.js
@@ -0,0 +1,12 @@
+export default class Navbar {
+static clickOnLogo() {
+    cy.get('.brand').click()
+}
+static search(text) {
+    cy.get('#searchTerm').type(`${text} {enter}`)
+}
+static clickSignIn() {
+    cy.get('#signin_button').click()
+}
+
+}
